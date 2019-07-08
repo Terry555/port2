aaf90967a6ba093e295dc9f0c6dc0485f2a7e683
github username <github email address>
2019-07-07 23:14:59 -0400

    changing

--- config/website.js
+++ config/website.js
@@ -3,9 +3,9 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
+  siteTitle: 'Terry - Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Terry', // Alternative Site title for SEO
+  siteTitleShort: 'Terry', // short_name for manifest
   siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
   siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
